ac84ab061c4d16b719c709ad917bbd5a7911081a
Vladimir <chappiedota@gmail.com>
2022-08-09 17:41:35 +0300

    front start

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,5 +1,7 @@
 /* eslint-disable import/no-unresolved */
 /* eslint-disable import/extensions */
+import React from 'react';
+
 import {
   BrowserRouter, NavLink, Route, Routes,
 } from 'react-router-dom';
@@ -7,26 +9,27 @@ import './App.css';
 import Auth from './components/Auth/Auth';
 import PersonalPage from './components/PersonalPage/PersonalPage';
 import Canvas from './components/Canvas/Canvas';
+import { useAppSelector } from './hooks/redux';
 
 function App() {
   return (
     <BrowserRouter>
-    <header>
-      <div>
-        <NavLink to="/auth">Auth</NavLink>
-        < br/>
-        <NavLink to='/canvas'>Paint</NavLink>
-        < br/>
-        <NavLink to='/personal'>Personal</NavLink>
-      </div>
-    </header>
-    <main>
-      <Routes>
-        <Route path={'/auth'} element={<Auth />} />
-        <Route path={'/personal'} element={<PersonalPage />} />
-        <Route path={'/canvas'} element={<Canvas />} />
-      </Routes>
-    </main>
+      <header>
+        <div>
+          <NavLink to="/auth">Auth</NavLink>
+          <br />
+          <NavLink to="/canvas">Paint</NavLink>
+          <br />
+          <NavLink to="/personal">Personal</NavLink>
+        </div>
+      </header>
+      <main>
+        <Routes>
+          <Route path="/auth" element={<Auth />} />
+          <Route path="/personal" element={<PersonalPage />} />
+          <Route path="/canvas" element={<Canvas />} />
+        </Routes>
+      </main>
     </BrowserRouter>
   );
 }
