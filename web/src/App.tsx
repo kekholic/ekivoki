2f7349d73a587ac52bc7dcf5033a8c0ffc730aee
IvanM <mahankovage@gmail.com>
2022-08-09 08:53:21 +0300

    nav

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,12 +1,25 @@
 import React from 'react';
 import logo from './logo.svg';
+import { BrowserRouter, NavLink, Route, Routes } from 'react-router-dom'
 import './App.css';
+import Auth from './components/Auth/Auth';
+import PersonalPage from './components/PersonalPage/PersonalPage';
+import Canvas from './components/Canvas/Canvas';
 
 function App() {
   return (
-    <div className="App">
-
-    </div>
+    <BrowserRouter>
+      <div>
+        <NavLink to="/auth">Auth</NavLink>
+        <NavLink to='/canvas'>Paint</NavLink>
+        <NavLink to='/personal'>Personal</NavLink>
+      </div>
+      <Routes>
+        <Route path={'/auth'} element={<Auth />} />
+        <Route path={'/personal'} element={<PersonalPage />} />
+        <Route path={'/canvas'} element={<Canvas />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
