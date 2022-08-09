d3895e63bf77109e8478a0b377209436ee4c221b
IvanM <mahankovage@gmail.com>
2022-08-09 12:11:12 +0300

    done

--- web/src/components/NavBar/NavBar.tsx
+++ web/src/components/NavBar/NavBar.tsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
 
 type Props = {}
 
 export default function NavBar({ }: Props) {
   return (
-    <div>NavBar</div>
+    <header>
+      <NavLink to="/auth">Auth</NavLink>
+      <NavLink to='/canvas'>Paint</NavLink>
+      <NavLink to='/personal'>Personal</NavLink>
+    </header>
   )
 }
