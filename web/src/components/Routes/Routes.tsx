42fdb08c5ef71f5d55b7eb99fc5fbee85ce23b9e
IvanM <mahankovage@gmail.com>
2022-08-11 15:23:32 +0300

    more

--- web/src/components/Routes/Routes.tsx
+++ web/src/components/Routes/Routes.tsx
@@ -4,12 +4,14 @@ import Canvas from '../Canvas/Canvas.jsx';
 import Registration from '../Auth/Registration/Registration';
 import Login from '../Auth/Login/Login';
 import GameStart from '../GameStart/GameStart';
+import GameMain from '../GameMain/GameMain';
 
 export const privateRoutes = [
   { path: '/game/start', element: <GameStart /> },
   { path: '/personal', element: <PersonalPage /> },
   { path: '/canvas', element: <Canvas /> },
   { path: '/logout' },
+  { path: '/game/:id', element: <GameMain /> },
 ];
 
 export const publicRoutes = [
