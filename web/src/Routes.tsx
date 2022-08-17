8d682f8c0aa61908b0d82648e978270c8b4ccae9
IvanM <mahankovage@gmail.com>
2022-08-17 21:54:58 +0300

    canvas:done

--- web/src/Routes.tsx
+++ web/src/Routes.tsx
@@ -5,7 +5,7 @@ import {
 import Activate from './components/Auth/Activate/Activate';
 import Login from './components/Auth/Login/Login';
 import Registration from './components/Auth/Registration/Registration';
-import Canvas from './components/Canvas/Canvas.jsx';
+import Canvas from './components/Canvas/Canvas';
 import GameInit from './components/GameInit/GameInit';
 import GameMain from './components/GameMain/GameMain';
 import GameList from './components/GameStart/GameList/GameList';
